e8b857a16d61ca53e615e532eec3923f38d5faf1
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-03 15:30:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
+import EditPostForm from './features/posts/EditPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+import NotificationsList from './features/notifications/NotificationsList';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+export default function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <h1>GenZ</h1>
+        <nav>
+          <a href="/">Posts</a> | <a href="/users">Users</a> | <a href="/notifications">Notifications</a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PostsList />} />
+          <Route path="/posts/:postId" element={<SinglePostPage />} />
+          <Route path="/editPost/:postId" element={<EditPostForm />} />
+          <Route path="/users" element={<UsersList />} />
+          <Route path="/users/:userId" element={<UserPage />} />
+          <Route path="/notifications" element={<NotificationsList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-
-export default App
