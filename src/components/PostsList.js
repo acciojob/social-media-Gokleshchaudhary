0d0656ff15b18b161e15080ae658a424177ce31c
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-03 15:59:13 +0530

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,15 +1,23 @@
-import React from 'react'
+// src/features/posts/PostsList.js
+import React from 'react';
 
 const PostsList = () => {
   return (
     <section className="posts-list">
       <div className="post">
         <h3>Sample Post</h3>
-        <p>This is a placeholder post.</p>
+        <p>This is a sample post content.</p>
+        <div>
+          <button>👍</button>
+          <button>❤️</button>
+          <button>😂</button>
+          <button>😢</button>
+          <button>😡</button>
+        </div>
+        <button className="button">Edit</button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default PostsList
-
+export default PostsList;
