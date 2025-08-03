cd54b98e72d5ac02dd06e2cacdb3db683ac579a6
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-03 15:38:15 +0530

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,1 +1,14 @@
+// src/features/posts/PostsList.js
+import React from 'react';
+
+const PostsList = () => {
+  return (
+    <div className="posts-list">
+      <h2>Posts</h2>
+      {/* Render posts here */}
+    </div>
+  );
+};
+
+export default PostsList;
 
