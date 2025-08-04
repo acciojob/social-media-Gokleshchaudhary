b91ee804b33c4b8cb4a22d69fc6fa064747628d3
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 11:26:58 +0530

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { postsState, addReaction } from '../data/posts';
-import { useSyncExternalStore } from 'use-sync-external-store';
 
 export default function PostsList() {
-  const posts = useSyncExternalStore(postsState.subscribe, postsState.get);
   return (
     <section className="posts-list">
-      {posts.map(post => (
-        <div className="post" key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-          {[...post.reactions, 0].map((r, idx) => (
-            <button key={idx} onClick={() => reactionsControlled(idx, post.id)}>
-              {r}
-            </button>
-          ))}
-          <button className="button">
-            <Link to={`/posts/${post.id}`}>View</Link>
-          </button>
-        </div>
-      ))}
+      <h2>Posts</h2>
     </section>
   );
-
-  function reactionsControlled(idx, postId) {
-    if (idx < 4) addReaction(postId, idx);
-  }
 }
