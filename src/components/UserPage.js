a8c644cce455f748a617c54ab040706663ed0e09
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 11:18:41 +0530

    Update UserPage.js

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { postsState } from '../data/posts';
+import { useSyncExternalStore } from 'use-sync-external-store';
+
+export default function UserPage() {
+  const { userId } = useParams();
+  const posts = useSyncExternalStore(postsState.subscribe, postsState.get);
+  const userPosts = posts.filter(p => p.authorId === Number(userId));
+  return (
+    <div>
+      {userPosts.map(p => (
+        <div key={p.id} className="post">
+          <h3>{p.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
 
