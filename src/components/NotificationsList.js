601e490cde226e5ee3e497f6fd526ad906bfbabb
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 11:19:12 +0530

    Update NotificationsList.js

--- src/components/NotificationsList.js
+++ src/components/NotificationsList.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
+import { notificationsState, fetchNotifications } from '../data/notifications';
+import { useSyncExternalStore } from 'use-sync-external-store';
+
+export default function NotificationsList() {
+  const [fetched, setFetched] = useState(false);
+  const notes = useSyncExternalStore(notificationsState.subscribe, notificationsState.get);
+
+  const onRefresh = () => {
+    setFetched(true);
+    fetchNotifications();
+  };
+
+  return (
+    <section className="notificationsList">
+      <button className="button" onClick={onRefresh}>Refresh Notifications</button>
+      {fetched && notes.map(n => <div key={n.id}>{n.message}</div>)}
+    </section>
+  );
+}
 
