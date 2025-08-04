e0167dc553a73ce1e01c1812701ca28b4c295a88
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 11:11:43 +0530

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,23 +1,30 @@
-// src/features/posts/PostsList.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { postsState, addReaction } from '../data/posts';
+import { useSyncExternalStore } from 'use-sync-external-store';
 
-const PostsList = () => {
+export default function PostsList() {
+  const posts = useSyncExternalStore(postsState.subscribe, postsState.get);
   return (
     <section className="posts-list">
-      <div className="post">
-        <h3>Sample Post</h3>
-        <p>This is a sample post content.</p>
-        <div>
-          <button>👍</button>
-          <button>❤️</button>
-          <button>😂</button>
-          <button>😢</button>
-          <button>😡</button>
+      {posts.map(post => (
+        <div className="post" key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          {[...post.reactions, 0].map((r, idx) => (
+            <button key={idx} onClick={() => reactionsControlled(idx, post.id)}>
+              {r}
+            </button>
+          ))}
+          <button className="button">
+            <Link to={`/posts/${post.id}`}>View</Link>
+          </button>
         </div>
-        <button className="button">Edit</button>
-      </div>
+      ))}
     </section>
   );
-};
 
-export default PostsList;
+  function reactionsControlled(idx, postId) {
+    if (idx < 4) addReaction(postId, idx);
+  }
+}
