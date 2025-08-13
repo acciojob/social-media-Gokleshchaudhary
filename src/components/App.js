1947c8cb2ead17fe4711861e9cab96fe81916ec5
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 21:24:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import PostsList from './features/posts/PostsList';
-import AddPostForm from './features/posts/AddPostForm';
-import SinglePostPage from './features/posts/SinglePostPage';
-import EditPostForm from './features/posts/EditPostForm';
-import UsersList from './features/users/UsersList';
-import UserPage from './features/users/UserPage';
-import NotificationsList from './features/notifications/NotificationsList';
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>GenZ</h1>
-      <nav>
-        <a href="/">Posts</a> | <a href="/users">Users</a> | <a href="/notifications">Notifications</a>
-      </nav>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <AddPostForm />
-            <PostsList />
-          </>
-        } />
-        <Route path="/posts/:postId" element={<SinglePostPage />} />
-        <Route path="/editPost/:postId" element={<EditPostForm />} />
-        <Route path="/users" element={<UsersList />} />
-        <Route path="/users/:userId" element={<UserPage />} />
-        <Route path="/notifications" element={<NotificationsList />} />
-      </Routes>
+    <div>
+      <h1>Shopping Cart</h1>
+      <p>Welcome to the shopping cart app!</p>
     </div>
   );
 }
+
+export default App;
