4ebced450db006920ee44de29179b7e08895c82e
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 21:40:00 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+      <Link to="/add">Add Post</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
 
