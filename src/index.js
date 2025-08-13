83e5cfdfa72ab94e520c4c303238ab31ea255075
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 20:40:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import { store } from './app/store'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import store from './app/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
     </Router>
   </Provider>
-)
+);
