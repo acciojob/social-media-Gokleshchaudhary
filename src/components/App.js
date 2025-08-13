45a22f5b7a22413932a5df59e2f78bc545b2e346
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 20:42:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import PostsList from './features/posts/PostsList'
-import AddPostForm from './features/posts/AddPostForm'
-import SinglePostPage from './features/posts/SinglePostPage'
-import EditPostForm from './features/posts/EditPostForm'
-import UsersList from './features/users/UsersList'
-import UserPage from './features/users/UserPage'
-import NotificationsList from './features/notifications/NotificationsList'
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+import NotificationsList from './features/notifications/NotificationsList';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>GenZ</h1>
-      <Navbar />
+      <nav>
+        <a href="/">Posts</a> | <a href="/users">Users</a> | <a href="/notifications">Notifications</a>
+      </nav>
       <Routes>
-        <Route path="/" element={<><AddPostForm /><PostsList /></>} />
+        <Route path="/" element={
+          <>
+            <AddPostForm />
+            <PostsList />
+          </>
+        } />
         <Route path="/posts/:postId" element={<SinglePostPage />} />
         <Route path="/editPost/:postId" element={<EditPostForm />} />
         <Route path="/users" element={<UsersList />} />
@@ -23,7 +29,5 @@ function App() {
         <Route path="/notifications" element={<NotificationsList />} />
       </Routes>
     </div>
-  )
+  );
 }
-
-export default App
