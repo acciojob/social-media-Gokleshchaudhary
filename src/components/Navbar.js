f319a0458ca94a36b3d6cfd42eb1bf41ec5318af
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 20:37:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="/">Posts</a>
+      {' | '}
+      <a href="/users">Users</a>
+      {' | '}
+      <a href="/notifications">Notifications</a>
+    </nav>
+  )
+}
+
+export default Navbar
 
