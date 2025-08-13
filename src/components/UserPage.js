42b3dba00ae8473ff0f281f4f731a66a8b26caae
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 20:47:42 +0530

    Update UserPage.js

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
+
+export default function UserPage() {
+  const { userId } = useParams();
+  const user = useSelector(state => state.users.find(u => u.id === userId));
+  const posts = useSelector(state => state.posts.filter(p => p.user === userId));
+
+  return (
+    <section>
+      <h2>{user?.name}</h2>
+      {posts.map(p => (
+        <article key={p.id} className="post">
+          <h3>{p.title}</h3>
+          <p>{p.content}</p>
+          <Link to={`/posts/${p.id}`} className="button">View Post</Link>
+        </article>
+      ))}
+    </section>
+  );
+}
 
