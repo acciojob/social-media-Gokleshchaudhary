2a2a439379131cbca523d69348704dd02ce925ef
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 21:33:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import PostList from './PostList';
+import AddPost from './AddPost';
 
 function App() {
   return (
     <div>
-      <h1>Shopping Cart</h1>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={PostList} />
+        <Route path="/add" component={AddPost} />
+      </Switch>
     </div>
   );
 }
