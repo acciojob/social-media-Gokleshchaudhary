3558f41e08ddfd1ef3225b3870e2d75a93657f86
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-13 21:39:41 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const PostList = () => {
+  const posts = useSelector((state) => state.posts);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Posts</h2>
+      {posts.length === 0 ? (
+        <p>No posts available</p>
+      ) : (
+        <ul>
+          {posts.map((post, index) => (
+            <li key={index}>{post}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PostList;
 
